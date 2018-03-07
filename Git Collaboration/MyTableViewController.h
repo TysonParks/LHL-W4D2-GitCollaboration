76a107d867c175e7e84cc8ccdbe55f3ac574544c
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-03-06 21:29:38 -0800

    hey

---
+++ Git Collaboration/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  Git Collaboration
+//
+//  Created by Chris Dean on 2018-03-06.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
