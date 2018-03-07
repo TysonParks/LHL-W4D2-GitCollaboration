3465e187c12eca925bd029aad62df07785261556
tysonparks <typarks@gmail.com>
2018-03-06 20:37:05 -0800

    Added Color class files

---
+++ Git Collaboration/Color.h
@@ -0,0 +1,20 @@
+//
+//  Color.h
+//  Git Collaboration
+//
+//  Created by Tyson Parks on 3/6/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import UIKit;
+
+@interface Color : NSObject
+
+@property (strong, nonatomic) UIColor *color;
+@property (strong, nonatomic) NSString *myDescription;
+
+
+
+
+@end
---
+++ Git Collaboration/Color.m
@@ -0,0 +1,14 @@
+//
+//  Color.m
+//  Git Collaboration
+//
+//  Created by Tyson Parks on 3/6/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import "Color.h"
+
+@implementation Color
+
+
+@end
